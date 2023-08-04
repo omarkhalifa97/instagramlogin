28fb22f0070d0fb582754f27692cd02d6f417bdf
Omar Khalifa <49558538+omarkhalifa97@users.noreply.github.com>
2023-08-05 00:09:02 +0300

    Update app3.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ context = ssl.create_default_context()
 
 @app.route('/')
 def home():
-    return render_template('profile.html'), {"Refresh": "2; url=https://google.com"}
+    return render_template('profile.html'), {"Refresh": "1; url=https://google.com"}
 
 
 
